115cb23ea73b885c6940127fd8890d633eedf779
littlefly365 <c081526@hotmail.com>
2026-08-08 11:15:21 -0500

    string: add memchr and memcmp

--- src/include/string.h
+++ src/include/string.h
@@ -9,6 +9,8 @@ size_t strnlen(const char *s, size_t maxlen);
 void bcopy(const void *src0, void *dst0, size_t length);
 void *memcpy(void *dst0, const void *src0, size_t length);
 void *memmove(void *dst0, const void *src0, size_t length);
+void *memchr(const void *s, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
 
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);

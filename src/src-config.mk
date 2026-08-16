e9926e0c28267ac906942d35103cf6edfd3a73b8
littlefly365 <c081526@hotmail.com>
2026-08-16 13:08:31 -0500

    src-config.mk: remove STDIO from SRCS (it has not been ported)

--- src/src-config.mk
+++ src/src-config.mk
@@ -3,11 +3,10 @@ CTYPE := $(wildcard ctype/*.c)
 SYSCALL := $(wildcard syscall/*.c)
 GEN := $(wildcard gen/*.c)
 STDLIB := $(wildcard stdlib/*.c)
-STDIO := $(wildcard stdio/*.c)
 
 GENERAL_INCLUDES := $(wildcard include/*.h)
 SYSTEM_INCLUDES := $(wildcard include/sys/*.h)
 MACHINE_INCLUDES := $(wildcard arch/$(MACHINE)/machine/*.h)
 
-ALL_SRCS := $(STRING) $(CTYPE) $(SYSCALL) $(GEN) $(STDLIB) $(STDIO)
+ALL_SRCS := $(STRING) $(CTYPE) $(SYSCALL) $(GEN) $(STDLIB)
 ALL_OBJS := $(ALL_SRCS:%.c=%.o)

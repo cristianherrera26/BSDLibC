3ab37acf2b03442a3316ee4bf22a8a6bbcc8cb91
littlefly365 <c081526@hotmail.com>
2026-08-19 11:37:25 -0500

    crt: move libc_start.c to libc

--- src/src-config.mk
+++ src/src-config.mk
@@ -4,11 +4,12 @@ LINUX := $(wildcard linux/*.c)
 GEN := $(wildcard gen/*.c)
 STDLIB := $(wildcard stdlib/*.c)
 MD := $(wildcard md/*.c)
+LIB := $(wildcard crt/libc_start.c)
 COMPAT := $(wildcard compat/OpenBSD/*.c) $(wildcard compat/NetBSD/*.c)
 
 GENERAL_INCLUDES := $(wildcard include/*.h)
 SYSTEM_INCLUDES := $(wildcard include/sys/*.h)
 MACHINE_INCLUDES := $(wildcard arch/$(MACHINE)/machine/*.h)
 
-ALL_SRCS := $(STRING) $(CTYPE) $(LINUX) $(GEN) $(STDLIB) $(MD) $(COMPAT)
+ALL_SRCS := $(STRING) $(CTYPE) $(LINUX) $(GEN) $(STDLIB) $(MD) $(LIB) $(COMPAT)
 ALL_OBJS := $(ALL_SRCS:%.c=%.o)
